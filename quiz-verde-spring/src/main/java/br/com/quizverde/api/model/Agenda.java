133ca3937af8a30374a88bb6ff0de298045aba20
alci.barros <alcirbarros@hotmail.com.br>
2020-04-05 19:50:31 -0300

    criando modelo Agenda

---
+++ quiz-verde-spring/src/main/java/br/com/quizverde/api/model/Agenda.java
@@ -0,0 +1,41 @@
+package br.com.quizverde.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "agn_agenda")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Agenda implements Serializable {
+
+    @Id
+    @Column(name = "agn_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "agn_nome_medico")
+    private String nomeMedico;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+}
